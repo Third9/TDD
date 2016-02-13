fd2c580473411fd2661f6285d91721d15195cf14
KwonH <dev.third9@gmail.com>
2016-02-13 15:22:10 +0900

    POST 후에 리다이렉션과 템플릿에 모든 작업 아이템 표시

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
+from lists.models import Item
 
 class HomePageTest(TestCase):
 
@@ -28,16 +29,35 @@ class HomePageTest(TestCase):
 
         response = home_page(request)
 
-        self.assertIn("신규 작업 아이템", response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text':'신규 작업 아이템'}
-        )
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "신규 작업 아이템")
 
-        self.assertEqual(response.content.decode(), expected_html)
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = "신규 작업 아이템"
 
+        response = home_page(request)
 
-from lists.models import Item
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text="dummy 1")
+        Item.objects.create(text="dummy 2")
+
+        request = HttpRequest()
+
+        response = home_page(request)
+
+        self.assertIn("dummy 1", response.content.decode())
+        self.assertIn("dummy 2", response.content.decode())
 
 class ItemModelTest(TestCase):
 
--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,17 @@
 # -*- coding: utf8 -*-
 
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+# from django.http import HttpResponse
+
+from lists.models import Item
 
 
 def home_page(request):
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', '')
-    })
-    # return HttpResponse('<html><title>To-Do lists</title></html>')
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST.get('item_text', ''))
+        return redirect('/')
+
+    items = Item.objects.all()
+
+    return render(request, 'home.html', {'items':items})
+
